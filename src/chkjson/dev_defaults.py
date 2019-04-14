98724e222586d3b5d0e05ca887c7e47bcd296fd1
Seth-David Donald Dworman <sdworman@brandeis.edu>
2019-04-14 11:54:54 -0400

    keep track of default values for commonly used files and variables for local development

---
+++ src/chkjson/dev_defaults.py
@@ -0,0 +1,9 @@
+"""Default values for commonly used filepaths, directories, etc. for local development.
+
+"""
+
+# canonical location of maps for Starcraft Broodwar on macOS
+MAC_STARCRAFT_MAPS_DIR = '/Applications/StarCraft/Maps'
+
+if __name__ == '__main__':
+    pass
