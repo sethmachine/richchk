4db198543d78954bacbfbd6b73e9c0b483f0a6a9
Seth-David Donald Dworman <sdworman@brandeis.edu>
2019-04-14 11:54:01 -0400

    constants for unit ids, weapon ids, and mapping unit ids to each weapon id which that unit uses

---
+++ src/chkjson/section/constants/unit_ids.py
@@ -0,0 +1,458 @@
+TERRAN_MARINE = 0
+TERRAN_GHOST = 1
+TERRAN_VULTURE = 2
+TERRAN_GOLIATH = 3
+GOLIATH_TURRET = 4
+TERRAN_SIEGE_TANK_TANK_MODE = 5
+TANK_TURRET_TANK_MODE = 6
+TERRAN_SCV = 7
+TERRAN_WRAITH = 8
+TERRAN_SCIENCE_VESSEL = 9
+GUI_MONTANG_FIREBAT = 10
+TERRAN_DROPSHIP = 11
+TERRAN_BATTLECRUISER = 12
+VULTURE_SPIDER_MINE = 13
+NUCLEAR_MISSILE = 14
+TERRAN_CIVILIAN = 15
+SARAH_KERRIGAN_GHOST = 16
+ALAN_SCHEZAR_GOLIATH = 17
+ALAN_SCHEZAR_TURRET = 18
+JIM_RAYNOR_VULTURE = 19
+JIM_RAYNOR_MARINE = 20
+TOM_KAZANSKY_WRAITH = 21
+MAGELLAN_SCIENCE_VESSEL = 22
+EDMUND_DUKE_SIEGE_TANK = 23
+EDMUND_DUKE_TURRET = 24
+EDMUND_DUKE_SIEGE_MODE = 25
+EDMUND_DUKE_TURRET_SIEGE_MODE = 26
+ARCTURUS_MENGSK_BATTLECRUISER = 27
+HYPERION_BATTLECRUISER = 28
+NORAD_II_BATTLECRUISER = 29
+TERRAN_SIEGE_TANK_SIEGE_MODE = 30
+TANK_TURRET_SIEGE_MODE = 31
+FIREBAT = 32
+SCANNER_SWEEP = 33
+TERRAN_MEDIC = 34
+ZERG_LARVA = 35
+ZERG_EGG = 36
+ZERG_ZERGLING = 37
+ZERG_HYDRALISK = 38
+ZERG_ULTRALISK = 39
+ZERG_BROODLING = 40
+ZERG_DRONE = 41
+ZERG_OVERLORD = 42
+ZERG_MUTALISK = 43
+ZERG_GUARDIAN = 44
+ZERG_QUEEN = 45
+ZERG_DEFILER = 46
+ZERG_SCOURGE = 47
+TORRARSQUE_ULTRALISK = 48
+MATRIARCH_QUEEN = 49
+INFESTED_TERRAN = 50
+INFESTED_KERRIGAN = 51
+UNCLEAN_ONE_DEFILER = 52
+HUNTER_KILLER_HYDRALISK = 53
+DEVOURING_ONE_ZERGLING = 54
+KUKULZA_MUTALISK = 55
+KUKULZA_GUARDIAN = 56
+YGGDRASILL_OVERLORD = 57
+TERRAN_VALKYRIE_FRIGATE = 58
+MUTALISK_GUARDIAN_COCOON = 59
+PROTOSS_CORSAIR = 60
+PROTOSS_DARK_TEMPLAR_UNIT = 61
+ZERG_DEVOURER = 62
+PROTOSS_DARK_ARCHON = 63
+PROTOSS_PROBE = 64
+PROTOSS_ZEALOT = 65
+PROTOSS_DRAGOON = 66
+PROTOSS_HIGH_TEMPLAR = 67
+PROTOSS_ARCHON = 68
+PROTOSS_SHUTTLE = 69
+PROTOSS_SCOUT = 70
+PROTOSS_ARBITER = 71
+PROTOSS_CARRIER = 72
+PROTOSS_INTERCEPTOR = 73
+DARK_TEMPLAR_HERO = 74
+ZERATUL_DARK_TEMPLAR = 75
+TASSADAR_ZERATUL_ARCHON = 76
+FENIX_ZEALOT = 77
+FENIX_DRAGOON = 78
+TASSADAR_TEMPLAR = 79
+MOJO_SCOUT = 80
+WARBRINGER_REAVER = 81
+GANTRITHOR_CARRIER = 82
+PROTOSS_REAVER = 83
+PROTOSS_OBSERVER = 84
+PROTOSS_SCARAB = 85
+DANIMOTH_ARBITER = 86
+ALDARIS_TEMPLAR = 87
+ARTANIS_SCOUT = 88
+RHYNADON_BADLANDS_CRITTER = 89
+BENGALAAS_JUNGLE_CRITTER = 90
+UNUSED_WAS_CARGO_SHIP = 91
+UNUSED_WAS_MERCENARY_GUNSHIP = 92
+SCANTID_DESERT_CRITTER = 93
+KAKARU_TWILIGHT_CRITTER = 94
+RAGNASAUR_ASHWORLD_CRITTER = 95
+URSADON_ICE_WORLD_CRITTER = 96
+LURKER_EGG = 97
+RASZAGAL = 98
+SAMIR_DURAN_GHOST = 99
+ALEXEI_STUKOV_GHOST = 100
+MAP_REVEALER = 101
+GERARD_DUGALLE = 102
+ZERG_LURKER = 103
+INFESTED_DURAN = 104
+DISRUPTION_WEB = 105
+TERRAN_COMMAND_CENTER = 106
+TERRAN_COMSAT_STATION = 107
+TERRAN_NUCLEAR_SILO = 108
+TERRAN_SUPPLY_DEPOT = 109
+TERRAN_REFINERY = 110
+TERRAN_BARRACKS = 111
+TERRAN_ACADEMY = 112
+TERRAN_FACTORY = 113
+TERRAN_STARPORT = 114
+TERRAN_CONTROL_TOWER = 115
+TERRAN_SCIENCE_FACILITY = 116
+TERRAN_COVERT_OPS = 117
+TERRAN_PHYSICS_LAB = 118
+UNUSED_WAS_STARBASE = 119
+TERRAN_MACHINE_SHOP = 120
+UNUSED_WAS_REPAIR_BAY = 121
+TERRAN_ENGINEERING_BAY = 122
+TERRAN_ARMORY = 123
+TERRAN_MISSILE_TURRET = 124
+TERRAN_BUNKER = 125
+NORAD_II = 126
+ION_CANNON = 127
+URAJ_CRYSTAL = 128
+KHALIS_CRYSTAL = 129
+INFESTED_COMMAND_CENTER = 130
+ZERG_HATCHERY = 131
+ZERG_LAIR = 132
+ZERG_HIVE = 133
+ZERG_NYDUS_CANAL = 134
+ZERG_HYDRALISK_DEN = 135
+ZERG_DEFILER_MOUND = 136
+ZERG_GREATER_SPIRE = 137
+ZERG_QUEENS_NEST = 138
+ZERG_EVOLUTION_CHAMBER = 139
+ZERG_ULTRALISK_CAVERN = 140
+ZERG_SPIRE = 141
+ZERG_SPAWNING_POOL = 142
+ZERG_CREEP_COLONY = 143
+ZERG_SPORE_COLONY = 144
+UNUSED_ZERG_BUILDING = 145
+ZERG_SUNKEN_COLONY = 146
+ZERG_OVERMIND_WITH_SHELL = 147
+ZERG_OVERMIND = 148
+ZERG_EXTRACTOR = 149
+MATURE_CHRYSALIS = 150
+ZERG_CEREBRATE = 151
+ZERG_CEREBRATE_DAGGOTH = 152
+UNUSED_ZERG_BUILDING_5 = 153
+PROTOSS_NEXUS = 154
+PROTOSS_ROBOTICS_FACILITY = 155
+PROTOSS_PYLON = 156
+PROTOSS_ASSIMILATOR = 157
+UNUSED_PROTOSS_BUILDING = 158
+PROTOSS_OBSERVATORY = 159
+PROTOSS_GATEWAY = 160
+UNUSED_PROTOSS_BUILDING = 161
+PROTOSS_PHOTON_CANNON = 162
+PROTOSS_CITADEL_OF_ADUN = 163
+PROTOSS_CYBERNETICS_CORE = 164
+PROTOSS_TEMPLAR_ARCHIVES = 165
+PROTOSS_FORGE = 166
+PROTOSS_STARGATE = 167
+STASIS_CELL_PRISON = 168
+PROTOSS_FLEET_BEACON = 169
+PROTOSS_ARBITER_TRIBUNAL = 170
+PROTOSS_ROBOTICS_SUPPORT_BAY = 171
+PROTOSS_SHIELD_BATTERY = 172
+KHAYDARIN_CRYSTAL_FORMATION = 173
+PROTOSS_TEMPLE = 174
+XELNAGA_TEMPLE = 175
+MINERAL_FIELD_TYPE_1 = 176
+MINERAL_FIELD_TYPE_2 = 177
+MINERAL_FIELD_TYPE_3 = 178
+CAVE = 179
+CAVEIN = 180
+CANTINA = 181
+MINING_PLATFORM = 182
+INDEPENDANT_COMMAND_CENTER = 183
+INDEPENDANT_STARPORT = 184
+INDEPENDANT_JUMP_GATE = 185
+RUINS = 186
+KYADARIN_CRYSTAL_FORMATION = 187
+VESPENE_GEYSER = 188
+WARP_GATE = 189
+PSI_DISRUPTOR = 190
+ZERG_MARKER = 191
+TERRAN_MARKER = 192
+PROTOSS_MARKER = 193
+ZERG_BEACON = 194
+TERRAN_BEACON = 195
+PROTOSS_BEACON = 196
+ZERG_FLAG_BEACON = 197
+TERRAN_FLAG_BEACON = 198
+PROTOSS_FLAG_BEACON = 199
+POWER_GENERATOR = 200
+OVERMIND_COCOON = 201
+DARK_SWARM = 202
+FLOOR_MISSILE_TRAP = 203
+FLOOR_HATCH = 204
+LEFT_UPPER_LEVEL_DOOR = 205
+RIGHT_UPPER_LEVEL_DOOR = 206
+LEFT_PIT_DOOR = 207
+RIGHT_PIT_DOOR = 208
+FLOOR_GUN_TRAP = 209
+LEFT_WALL_MISSILE_TRAP = 210
+LEFT_WALL_FLAME_TRAP = 211
+RIGHT_WALL_MISSILE_TRAP = 212
+RIGHT_WALL_FLAME_TRAP = 213
+START_LOCATION = 214
+FLAG = 215
+YOUNG_CHRYSALIS = 216
+PSI_EMITTER = 217
+DATA_DISC = 218
+KHAYDARIN_CRYSTAL = 219
+MINERAL_CLUSTER_TYPE_1 = 220
+MINERAL_CLUSTER_TYPE_2 = 221
+PROTOSS_VESPENE_GAS_ORB_TYPE_1 = 222
+PROTOSS_VESPENE_GAS_ORB_TYPE_2 = 223
+ZERG_VESPENE_GAS_SAC_TYPE_1 = 224
+ZERG_VESPENE_GAS_SAC_TYPE_2 = 225
+TERRAN_VESPENE_GAS_TANK_TYPE_1 = 226
+TERRAN_VESPENE_GAS_TANK_TYPE_2 = 227
+ID_TO_UNIT = {
+ 0: "Terran Marine",
+ 1: "Terran Ghost",
+ 2: "Terran Vulture",
+ 3: "Terran Goliath",
+ 4: "Goliath Turret",
+ 5: "Terran Siege Tank (Tank Mode)",
+ 6: "Tank Turret(Tank Mode)",
+ 7: "Terran SCV",
+ 8: "Terran Wraith",
+ 9: "Terran Science Vessel",
+ 10: "Gui Montang (Firebat)",
+ 11: "Terran Dropship",
+ 12: "Terran Battlecruiser",
+ 13: "Vulture Spider Mine",
+ 14: "Nuclear Missile",
+ 15: "Terran Civilian",
+ 16: "Sarah Kerrigan (Ghost)",
+ 17: "Alan Schezar (Goliath)",
+ 18: "Alan Schezar Turret",
+ 19: "Jim Raynor (Vulture)",
+ 20: "Jim Raynor (Marine)",
+ 21: "Tom Kazansky (Wraith)",
+ 22: "Magellan (Science Vessel)",
+ 23: "Edmund Duke (Siege Tank)",
+ 24: "Edmund Duke Turret",
+ 25: "Edmund Duke (Siege Mode)",
+ 26: "Edmund Duke Turret",
+ 27: "Arcturus Mengsk (Battlecruiser)",
+ 28: "Hyperion (Battlecruiser)",
+ 29: "Norad II (Battlecruiser)",
+ 30: "Terran Siege Tank (Siege Mode)",
+ 31: "Tank Turret (Siege Mode)",
+ 32: "Firebat",
+ 33: "Scanner Sweep",
+ 34: "Terran Medic",
+ 35: "Zerg Larva",
+ 36: "Zerg Egg",
+ 37: "Zerg Zergling",
+ 38: "Zerg Hydralisk",
+ 39: "Zerg Ultralisk",
+ 40: "Zerg Broodling",
+ 41: "Zerg Drone",
+ 42: "Zerg Overlord",
+ 43: "Zerg Mutalisk",
+ 44: "Zerg Guardian",
+ 45: "Zerg Queen",
+ 46: "Zerg Defiler",
+ 47: "Zerg Scourge",
+ 48: "Torrarsque (Ultralisk)",
+ 49: "Matriarch (Queen)",
+ 50: "Infested Terran",
+ 51: "Infested Kerrigan",
+ 52: "Unclean One (Defiler)",
+ 53: "Hunter Killer (Hydralisk)",
+ 54: "Devouring One (Zergling)",
+ 55: "Kukulza (Mutalisk)",
+ 56: "Kukulza (Guardian)",
+ 57: "Yggdrasill (Overlord)",
+ 58: "Terran Valkyrie Frigate",
+ 59: "Mutalisk/Guardian Cocoon",
+ 60: "Protoss Corsair",
+ 61: "Protoss Dark Templar(Unit)",
+ 62: "Zerg Devourer",
+ 63: "Protoss Dark Archon",
+ 64: "Protoss Probe",
+ 65: "Protoss Zealot",
+ 66: "Protoss Dragoon",
+ 67: "Protoss High Templar",
+ 68: "Protoss Archon",
+ 69: "Protoss Shuttle",
+ 70: "Protoss Scout",
+ 71: "Protoss Arbiter",
+ 72: "Protoss Carrier",
+ 73: "Protoss Interceptor",
+ 74: "Dark Templar(Hero)",
+ 75: "Zeratul (Dark Templar)",
+ 76: "Tassadar/Zeratul (Archon)",
+ 77: "Fenix (Zealot)",
+ 78: "Fenix (Dragoon)",
+ 79: "Tassadar (Templar)",
+ 80: "Mojo (Scout)",
+ 81: "Warbringer (Reaver)",
+ 82: "Gantrithor (Carrier)",
+ 83: "Protoss Reaver",
+ 84: "Protoss Observer",
+ 85: "Protoss Scarab",
+ 86: "Danimoth (Arbiter)",
+ 87: "Aldaris (Templar)",
+ 88: "Artanis (Scout)",
+ 89: "Rhynadon (Badlands Critter)",
+ 90: "Bengalaas (Jungle Critter)",
+ 91: "Unused - Was Cargo Ship",
+ 92: "Unused - Was Mercenary Gunship",
+ 93: "Scantid (Desert Critter)",
+ 94: "Kakaru (Twilight Critter)",
+ 95: "Ragnasaur (Ashworld Critter)",
+ 96: "Ursadon (Ice World Critter)",
+ 97: "Lurker Egg",
+ 98: "Raszagal",
+ 99: "Samir Duran (Ghost)",
+ 100: "Alexei Stukov (Ghost)",
+ 101: "Map Revealer",
+ 102: "Gerard DuGalle",
+ 103: "Zerg Lurker",
+ 104: "Infested Duran",
+ 105: "Disruption Web",
+ 106: "Terran Command Center",
+ 107: "Terran Comsat Station",
+ 108: "Terran Nuclear Silo",
+ 109: "Terran Supply Depot",
+ 110: "Terran Refinery",
+ 111: "Terran Barracks",
+ 112: "Terran Academy",
+ 113: "Terran Factory",
+ 114: "Terran Starport",
+ 115: "Terran Control Tower",
+ 116: "Terran Science Facility",
+ 117: "Terran Covert Ops",
+ 118: "Terran Physics Lab",
+ 119: "Unused - Was Starbase?",
+ 120: "Terran Machine Shop",
+ 121: "Unused - Was Repair Bay?",
+ 122: "Terran Engineering Bay",
+ 123: "Terran Armory",
+ 124: "Terran Missile Turret",
+ 125: "Terran Bunker",
+ 126: "Norad II",
+ 127: "Ion Cannon",
+ 128: "Uraj Crystal",
+ 129: "Khalis Crystal",
+ 130: "Infested Command Center",
+ 131: "Zerg Hatchery",
+ 132: "Zerg Lair",
+ 133: "Zerg Hive",
+ 134: "Zerg Nydus Canal",
+ 135: "Zerg Hydralisk Den",
+ 136: "Zerg Defiler Mound",
+ 137: "Zerg Greater Spire",
+ 138: "Zerg Queen's Nest",
+ 139: "Zerg Evolution Chamber",
+ 140: "Zerg Ultralisk Cavern",
+ 141: "Zerg Spire",
+ 142: "Zerg Spawning Pool",
+ 143: "Zerg Creep Colony",
+ 144: "Zerg Spore Colony",
+ 145: "Unused Zerg Building",
+ 146: "Zerg Sunken Colony",
+ 147: "Zerg Overmind (With Shell)",
+ 148: "Zerg Overmind",
+ 149: "Zerg Extractor",
+ 150: "Mature Chrysalis",
+ 151: "Zerg Cerebrate",
+ 152: "Zerg Cerebrate Daggoth",
+ 153: "Unused Zerg Building 5",
+ 154: "Protoss Nexus",
+ 155: "Protoss Robotics Facility",
+ 156: "Protoss Pylon",
+ 157: "Protoss Assimilator",
+ 158: "Unused Protoss Building",
+ 159: "Protoss Observatory",
+ 160: "Protoss Gateway",
+ 161: "Unused Protoss Building",
+ 162: "Protoss Photon Cannon",
+ 163: "Protoss Citadel of Adun",
+ 164: "Protoss Cybernetics Core",
+ 165: "Protoss Templar Archives",
+ 166: "Protoss Forge",
+ 167: "Protoss Stargate",
+ 168: "Stasis Cell/Prison",
+ 169: "Protoss Fleet Beacon",
+ 170: "Protoss Arbiter Tribunal",
+ 171: "Protoss Robotics Support Bay",
+ 172: "Protoss Shield Battery",
+ 173: "Khaydarin Crystal Formation",
+ 174: "Protoss Temple",
+ 175: "Xel'Naga Temple",
+ 176: "Mineral Field (Type 1)",
+ 177: "Mineral Field (Type 2)",
+ 178: "Mineral Field (Type 3)",
+ 179: "Cave",
+ 180: "Cave-in",
+ 181: "Cantina",
+ 182: "Mining Platform",
+ 183: "Independant Command Center",
+ 184: "Independant Starport",
+ 185: "Independant Jump Gate",
+ 186: "Ruins",
+ 187: "Kyadarin Crystal Formation",
+ 188: "Vespene Geyser",
+ 189: "Warp Gate",
+ 190: "PSI Disruptor",
+ 191: "Zerg Marker",
+ 192: "Terran Marker",
+ 193: "Protoss Marker",
+ 194: "Zerg Beacon",
+ 195: "Terran Beacon",
+ 196: "Protoss Beacon",
+ 197: "Zerg Flag Beacon",
+ 198: "Terran Flag Beacon",
+ 199: "Protoss Flag Beacon",
+ 200: "Power Generator",
+ 201: "Overmind Cocoon",
+ 202: "Dark Swarm",
+ 203: "Floor Missile Trap",
+ 204: "Floor Hatch",
+ 205: "Left Upper Level Door",
+ 206: "Right Upper Level Door",
+ 207: "Left Pit Door",
+ 208: "Right Pit Door",
+ 209: "Floor Gun Trap",
+ 210: "Left Wall Missile Trap",
+ 211: "Left Wall Flame Trap",
+ 212: "Right Wall Missile Trap",
+ 213: "Right Wall Flame Trap",
+ 214: "Start Location",
+ 215: "Flag",
+ 216: "Young Chrysalis",
+ 217: "Psi Emitter",
+ 218: "Data Disc",
+ 219: "Khaydarin Crystal",
+ 220: "Mineral Cluster Type 1",
+ 221: "Mineral Cluster Type 2",
+ 222: "Protoss Vespene Gas Orb Type 1",
+ 223: "Protoss Vespene Gas Orb Type 2",
+ 224: "Zerg Vespene Gas Sac Type 1",
+ 225: "Zerg Vespene Gas Sac Type 2",
+ 226: "Terran Vespene Gas Tank Type 1",
+ 227: "Terran Vespene Gas Tank Type 2"
+}
