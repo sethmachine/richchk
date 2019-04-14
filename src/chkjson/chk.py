6c50b4b16a368d0571e6ea02bf5205f53eef0568
Seth-David Donald Dworman <sdworman@brandeis.edu>
2019-04-14 11:51:12 -0400

    give each chk section a pointer to the chkstr section for adding new strings data

--- src/chkjson/chk.py
+++ src/chkjson/chk.py
@@ -6,12 +6,14 @@ See: http://www.starcraftai.com/wiki/CHK_Format
 
 import collections
 import dataclasses
+import io
 import os
 import struct
 import typing
 
 from . import logger
 from .section.abstract_chksection import ChkSection
+from .section.chkstr import ChkStr
 from .section.chkunk import ChkUnk
 from .section.name2section import name2section
 
@@ -20,6 +22,15 @@ from .section.name2section import name2section
 class Chk:
     sections: typing.List[ChkSection]
     name2sections: typing.Dict[str, typing.List[ChkSection]]
+    log: logger.Logger = None
+
+    def __post_init__(self):
+        # give each section a pointer to the string table
+        str_ = self.get_sections(ChkStr.name)[0]
+        for section in self.sections:
+            if hasattr(section, 'str_'):
+                section.str_ = str_
+        self.log = logger.get_log(self.__class__.__name__)
 
     def get_sections(self, name) -> typing.List[ChkSection]:
         return self.name2sections.get(name)
@@ -27,8 +38,16 @@ class Chk:
     def decompile(self, data: bytes):
         pass
 
-    def compile(self, data: typing.Dict):
-        pass
+    def compile(self):
+        """Compiles all CHK sections to create the new CHK, ready for use in .scx/.scm archive.
+
+        :return:
+        """
+        data = b''
+        for section in self.sections:
+            self.log.debug('Compiling {} section'.format(section.name))
+            data += section.compile(header=True)
+        return data
 
 
 class ChkReader:
@@ -74,6 +93,7 @@ class ChkReader:
                 # read name, size
                 # u32 Name - A 4-byte string uniquely identifying that chunk's purpose.
                 name = struct.unpack('4s', name_bytes)[0].decode('utf-8')
+                # name = struct.unpack('4s', name_bytes)[0]
                 # u32 Size - The size, in bytes, of the chunk (not including this header)
                 size = struct.unpack('I', f.read(4))[0]
                 section_data = f.read(size)
@@ -88,6 +108,34 @@ class ChkReader:
                 sections.append(sect)
                 name2sections[name].append(sect)
         return Chk(sections=sections, name2sections=name2sections)
+
+    def read_bytes(self, data: bytes):
+        sections = []
+        name2sections = collections.defaultdict(list)
+        f = io.BytesIO(data)
+        while True:
+            name_bytes = f.read(4)
+            if name_bytes == b'':
+                break
+            # read name, size
+            # u32 Name - A 4-byte string uniquely identifying that chunk's purpose.
+            name = struct.unpack('4s', name_bytes)[0].decode('utf-8')
+            # name = struct.unpack('4s', name_bytes)[0]
+            # u32 Size - The size, in bytes, of the chunk (not including this header)
+            size = struct.unpack('I', f.read(4))[0]
+            section_data = f.read(size)
+            section_class = name2section(name)
+            if section_class is ChkUnk:
+                self.log.warning('Unknown section: {}'.format(name))
+                sect = section_class.decompile(section_data)
+                sect.name = name
+            else:
+                sect = section_class.decompile(section_data)
+            sect.data = section_data
+            sections.append(sect)
+            name2sections[name].append(sect)
+        return Chk(sections=sections, name2sections=name2sections)
+
 
 
 class ChkWriter:
@@ -130,7 +178,33 @@ if __name__ == '__main__':
     c = chkr.read(infile)
     save_sections_to_dir(c.sections, '../../data/chk-sections')
     z = c.get_sections('STR ')[0]
+    str_ = c.get_sections('STR ')[0]
+    fd = c.compile()
     od = z.data
+    from .section.constants.unit_ids import *
+    z = c.get_sections('UNIx')[0]
+    for unid in range(len(ID_TO_UNIT)):
+        print(ID_TO_UNIT[unid], z.hitpoints[unid] / 256.0,
+              z.gas[unid])
+    from .section.chkunix import UnitSetting
+    from .section.constants.unit_ids import *
+    from .section.constants.weapon_ids import *
+    from .section.constants.unit_to_weapon_ids import *
+    un = c.get_sections('UNIS')[0]
+    # z.hitpoints[TORRARSQUE_ULTRALISK] = (45 * 256)
+    # un.hitpoints[TORRARSQUE_ULTRALISK] = (45 * 256)
+    # d1 = z.compile(header=False)
+    for unit in ID_TO_UNIT:
+        us = UnitSetting(unit, hitpoints=100, armor=100, name='ALLAH')
+        z.add_unit_setting(us)
+    newdata = c.compile()
+    outfile = '../../data/chk/demon_lore_yatapi_test_update_foobar.chk'
+    with open(outfile, 'wb') as f:
+        f.write(newdata)
+    zk = chkr.read(outfile)
+
+
+
     # a = z.compile(header=False)
     # # z.add_string('addme')
     # print(z.get_index('Nexus1'))
