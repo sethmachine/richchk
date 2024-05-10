d69fe65e54f491077bde7d01781934f7e971c003
sethmachine <sethmachine01@gmail.com>
2024-05-09 23:07:11 -0400

    create unit trigger action

--- test/transcoder/richchk/transcoders/richchk_trig_transcoder_test.py
+++ test/transcoder/richchk/transcoders/richchk_trig_transcoder_test.py
@@ -1,11 +1,22 @@
 import pytest
 
+from chkjson.io.richchk.lookups.mrgn.rich_mrgn_lookup_builder import (
+    RichMrgnLookupBuilder,
+)
+from chkjson.io.richchk.rich_str_lookup_builder import RichStrLookupBuilder
+from chkjson.model.chk.mrgn.decoded_mrgn_section import DecodedMrgnSection
+from chkjson.model.chk.str.decoded_str_section import DecodedStrSection
 from chkjson.model.chk.trig.decoded_trig_section import DecodedTrigSection
 from chkjson.model.richchk.mrgn.rich_mrgn_lookup import RichMrgnLookup
 from chkjson.model.richchk.richchk_decode_context import RichChkDecodeContext
 from chkjson.model.richchk.richchk_encode_context import RichChkEncodeContext
 from chkjson.model.richchk.str.rich_str_lookup import RichStrLookup
+from chkjson.transcoder.chk.transcoders.chk_mrgn_transcoder import ChkMrgnTranscoder
+from chkjson.transcoder.chk.transcoders.chk_str_transcoder import ChkStrTranscoder
 from chkjson.transcoder.chk.transcoders.chk_trig_transcoder import ChkTrigTranscoder
+from chkjson.transcoder.richchk.transcoders.richchk_mrgn_transcoder import (
+    RichChkMrgnTranscoder,
+)
 from chkjson.transcoder.richchk.transcoders.richchk_trig_transcoder import (
     RichChkTrigTranscoder,
 )
@@ -13,7 +24,7 @@ from chkjson.transcoder.richchk.transcoders.richchk_trig_transcoder import (
 from ....chk_resources import DEMON_LORE_CHK_SECTION_FILE_PATHS
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def real_decoded_trig() -> DecodedTrigSection:
     with open(
         DEMON_LORE_CHK_SECTION_FILE_PATHS[DecodedTrigSection.section_name().value], "rb"
@@ -22,33 +33,58 @@ def real_decoded_trig() -> DecodedTrigSection:
     return ChkTrigTranscoder().decode(chk_binary_data)
 
 
-def test_integration_it_decodes_and_encodes_back_to_chk_without_changing_data(
-    real_decoded_trig,
-):
-    # these contexts won't work once string IDs are resolved to RichString
-    # same with location IDs, CUWP IDs, and any other ID that gets de-referenced in the RichChk
-    rich_chk_decode_context = RichChkDecodeContext(
-        _rich_str_lookup=RichStrLookup(
-            _string_by_id_lookup={},
-            _id_by_string_lookup={},
+@pytest.fixture(scope="function")
+def real_str_lookup() -> RichStrLookup:
+    with open(
+        DEMON_LORE_CHK_SECTION_FILE_PATHS[DecodedStrSection.section_name().value], "rb"
+    ) as f:
+        chk_binary_data = f.read()
+    return RichStrLookupBuilder().build_lookup(
+        decoded_str_section=ChkStrTranscoder().decode(chk_binary_data)
+    )
+
+
+@pytest.fixture(scope="function")
+def real_mrgn_lookup(real_str_lookup) -> RichMrgnLookup:
+    with open(
+        DEMON_LORE_CHK_SECTION_FILE_PATHS[DecodedMrgnSection.section_name().value], "rb"
+    ) as f:
+        chk_binary_data = f.read()
+    return RichMrgnLookupBuilder().build_lookup(
+        RichChkMrgnTranscoder().decode(
+            ChkMrgnTranscoder().decode(chk_binary_data),
+            rich_chk_decode_context=RichChkDecodeContext(
+                _rich_str_lookup=real_str_lookup, _rich_mrgn_lookup=None
+            ),
         )
     )
-    rich_chk_encode_context = RichChkEncodeContext(
-        _rich_str_lookup=RichStrLookup(
-            _string_by_id_lookup={},
-            _id_by_string_lookup={},
-        ),
-        _rich_mrgn_lookup=RichMrgnLookup(
-            _location_by_id_lookup={}, _id_by_location_lookup={}
-        ),
+
+
+@pytest.fixture(scope="function")
+def real_rich_chk_decode_context(real_str_lookup, real_mrgn_lookup):
+    return RichChkDecodeContext(
+        _rich_str_lookup=real_str_lookup, _rich_mrgn_lookup=real_mrgn_lookup
     )
+
+
+@pytest.fixture(scope="function")
+def real_rich_chk_encode_context(real_str_lookup, real_mrgn_lookup):
+    return RichChkEncodeContext(
+        _rich_str_lookup=real_str_lookup, _rich_mrgn_lookup=real_mrgn_lookup
+    )
+
+
+def test_integration_it_decodes_and_encodes_back_to_chk_without_changing_data(
+    real_decoded_trig, real_rich_chk_decode_context, real_rich_chk_encode_context
+):
     rich_transcoder = RichChkTrigTranscoder()
     rich_trig = rich_transcoder.decode(
         real_decoded_trig,
-        rich_chk_decode_context=rich_chk_decode_context,
+        rich_chk_decode_context=real_rich_chk_decode_context,
     )
-    decoded_trig_again = rich_transcoder.encode(rich_trig, rich_chk_encode_context)
+    decoded_trig_again = rich_transcoder.encode(rich_trig, real_rich_chk_encode_context)
     assert decoded_trig_again == real_decoded_trig
     assert (
-        rich_transcoder.decode(decoded_trig_again, rich_chk_decode_context) == rich_trig
+        rich_transcoder.decode(decoded_trig_again, real_rich_chk_decode_context)
+        == rich_trig
     )
