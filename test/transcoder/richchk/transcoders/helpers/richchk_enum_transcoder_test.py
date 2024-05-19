3824115d9ceb8dc0e8efd01dc48af605cf4779bc
sethmachine <sethmachine01@gmail.com>
2024-05-19 13:27:19 -0400

    remove stateful unit test

--- test/transcoder/richchk/transcoders/helpers/richchk_enum_transcoder_test.py
+++ test/transcoder/richchk/transcoders/helpers/richchk_enum_transcoder_test.py
@@ -1,7 +1,6 @@
 import pytest
 
 from chkjson.model.richchk.trig.player_id import PlayerId
-from chkjson.model.richchk.unis.unit_id import UnitId
 from chkjson.transcoder.richchk.transcoders.helpers.richchk_enum_transcoder import (
     RichChkEnumTranscoder,
 )
@@ -10,19 +9,6 @@ from chkjson.transcoder.richchk.transcoders.helpers.richchk_enum_transcoder impo
 def test_it_decodes_id_to_enum_instance():
     player_1_id = 0
     assert RichChkEnumTranscoder.decode_enum(player_1_id, PlayerId) == PlayerId.PLAYER_1
-
-
-def test_it_builds_enum_id_map_after_first_usage():
-    terran_marine_id = 0
-    assert UnitId not in RichChkEnumTranscoder._ENUM_ID_MAP
-    assert (
-        RichChkEnumTranscoder.decode_enum(terran_marine_id, UnitId)
-        == UnitId.TERRAN_MARINE
-    )
-    assert UnitId in RichChkEnumTranscoder._ENUM_ID_MAP
-    assert set(RichChkEnumTranscoder._ENUM_ID_MAP[UnitId].values()) == set(
-        [unit_id for unit_id in UnitId]
-    )
 
 
 def test_it_tells_if_enum_id_exists():
