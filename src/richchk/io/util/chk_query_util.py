b9dee3fdab39f1d8883476c52d999af3474d890d
sethmachine <sethmachine01@gmail.com>
2024-06-24 18:10:30 -0400

    remove need to cast when finding decoded chk section

--- src/richchk/io/util/chk_query_util.py
+++ src/richchk/io/util/chk_query_util.py
@@ -8,40 +8,43 @@ from ...model.chk_section_name import ChkSectionName
 from ...model.richchk.rich_chk import RichChk
 from ...model.richchk.rich_chk_section import RichChkSection
 
-_T = TypeVar("_T", bound="RichChkSection", covariant=True)
+_T = TypeVar("_T", bound=RichChkSection, covariant=True)
+_U = TypeVar("_U", bound=DecodedChkSection, covariant=True)
 
 
 class ChkQueryUtil:
     @staticmethod
     def find_only_decoded_section_in_chk(
-        chk_section_name: ChkSectionName, chk: Union[DecodedChk, RichChk]
-    ) -> DecodedChkSection:
+        chk_section_type: Type[_U], chk: Union[DecodedChk, RichChk]
+    ) -> _U:
         """Find the only DecodedChkSection with the given name.
 
         Throws if the section does not exist or there is more than 1 section with the
         same name.
 
-        :param chk_section_name:
+        :param chk_section_type:
         :param chk:
         :return:
         """
         named_sections: list[Union[DecodedChkSection, RichChkSection]] = []
         if isinstance(chk, RichChk):
-            named_sections = chk.get_sections_by_name(chk_section_name=chk_section_name)
+            named_sections = chk.get_sections_by_name(
+                chk_section_name=chk_section_type.section_name()
+            )
         elif isinstance(chk, DecodedChk):
             decoded_chk_sections = chk.get_sections_by_name(
-                chk_section_name=chk_section_name
+                chk_section_name=chk_section_type.section_name()
             )
             named_sections += decoded_chk_sections
         if not named_sections:
             msg = (
-                f"The CHK has no {chk_section_name.value} sections present! "
+                f"The CHK has no {chk_section_type.section_name().value} sections present! "
                 "The CHK may not be valid.  Only pass in valid CHK data."
             )
             raise ValueError(msg)
         if len(named_sections) > 1:
             msg = (
-                f"The CHK has more than 1 {chk_section_name.value} sections present"
+                f"The CHK has more than 1 {chk_section_type.section_name().value} sections present"
                 f" when only 1 is expected!"
             )
             raise ValueError(msg)
@@ -49,8 +52,9 @@ class ChkQueryUtil:
         if isinstance(only_section, RichChkSection):
             raise ValueError(
                 f"Expected a DecodedChkSection but found a RichChkSection "
-                f"for section named {chk_section_name.value}"
+                f"for section named {chk_section_type.section_name().value}"
             )
+        assert isinstance(only_section, chk_section_type)
         return only_section
 
     @staticmethod
