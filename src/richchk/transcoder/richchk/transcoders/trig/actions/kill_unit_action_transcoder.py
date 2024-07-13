b8f9a0d5511f4aeacbb303e670d7e8c7ba02ed63
sethmachine <sethmachine01@gmail.com>
2024-07-13 16:09:58 -0400

    add trigger actions for kill and remove units

---
+++ src/richchk/transcoder/richchk/transcoders/trig/actions/kill_unit_action_transcoder.py
@@ -0,0 +1,58 @@
+"""Decode Victory trigger action."""
+
+from ......model.chk.trig.decoded_trigger_action import DecodedTriggerAction
+from ......model.richchk.richchk_decode_context import RichChkDecodeContext
+from ......model.richchk.richchk_encode_context import RichChkEncodeContext
+from ......model.richchk.trig.actions.kill_unit_action import KillUnitAction
+from ......model.richchk.trig.player_id import PlayerId
+from ......model.richchk.unis.unit_id import UnitId
+from ......util import logger
+from ...helpers.richchk_enum_transcoder import RichChkEnumTranscoder
+from ..rich_trigger_action_transcoder import RichTriggerActionTranscoder
+from ..rich_trigger_action_transcoder_factory import (
+    _RichTriggerActionRegistrableTranscoder,
+)
+
+
+class RichTriggerKillUnitActionTranscoder(
+    RichTriggerActionTranscoder[KillUnitAction, DecodedTriggerAction],
+    _RichTriggerActionRegistrableTranscoder,
+    trigger_action_id=KillUnitAction.action_id(),
+):
+    def __init__(self) -> None:
+        self.log = logger.get_logger(RichTriggerKillUnitActionTranscoder.__name__)
+
+    def _decode(
+        self,
+        decoded_action: DecodedTriggerAction,
+        rich_chk_decode_context: RichChkDecodeContext,
+    ) -> KillUnitAction:
+        assert decoded_action.action_id == KillUnitAction.action_id().id
+        return KillUnitAction(
+            _group=RichChkEnumTranscoder.decode_enum(
+                decoded_action.first_group, PlayerId
+            ),
+            _unit=RichChkEnumTranscoder.decode_enum(
+                decoded_action.action_argument_type, UnitId
+            ),
+        )
+
+    def _encode(
+        self,
+        rich_action: KillUnitAction,
+        rich_chk_encode_context: RichChkEncodeContext,
+    ) -> DecodedTriggerAction:
+        return DecodedTriggerAction(
+            _location_id=0,
+            _text_string_id=0,
+            _wav_string_id=0,
+            _time=0,
+            _first_group=RichChkEnumTranscoder.encode_enum(rich_action.group),
+            _second_group=0,
+            _action_argument_type=RichChkEnumTranscoder.encode_enum(rich_action.unit),
+            _action_id=rich_action.action_id().id,
+            _quantifier_or_switch_or_order=0,
+            _flags=0,
+            _padding=0,
+            _mask_flag=0,
+        )
