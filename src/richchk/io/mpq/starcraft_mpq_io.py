efe2233c3a54814c453c721c5320253303f2e8fc
sethmachine <sethmachine01@gmail.com>
2024-07-31 23:21:05 -0400

    expose wav metadata during RichChk encode to CHK

--- src/richchk/io/mpq/starcraft_mpq_io.py
+++ src/richchk/io/mpq/starcraft_mpq_io.py
@@ -7,7 +7,9 @@ from ...io.chk.chk_io import ChkIo
 from ...io.richchk.richchk_io import RichChkIo
 from ...model.mpq.stormlib.stormlib_archive_mode import StormLibArchiveMode
 from ...model.richchk.rich_chk import RichChk
+from ...model.richchk.wav.rich_wav_metadata_lookup import RichWavMetadataLookup
 from ...mpq.stormlib.stormlib_wrapper import StormLibWrapper
+from .starcraft_wav_metadata_io import StarcraftWavMetadataIo
 
 
 class StarcraftMpqIo:
@@ -44,14 +46,22 @@ class StarcraftMpqIo:
             raise FileNotFoundError(path_to_base_mpq_file)
         if os.path.exists(path_to_new_mpq_file) and not overwrite_existing:
             raise FileExistsError(
-                f"The output MPQ file {path_to_new_mpq_file} already exists."
+                f"Refusing to create new MPQ because it already exists: {path_to_new_mpq_file}"
             )
         with (
             tempfile.NamedTemporaryFile() as temp_chk_file,
             tempfile.NamedTemporaryFile() as temp_mpq_file,
         ):
+
             ChkIo().encode_chk_to_file(
-                RichChkIo().encode_chk(chk), temp_chk_file.name, force_create=True
+                RichChkIo().encode_chk(
+                    rich_chk=chk,
+                    wav_metadata_lookup=self._build_wav_metadata_lookup(
+                        path_to_base_mpq_file
+                    ),
+                ),
+                temp_chk_file.name,
+                force_create=True,
             )
             shutil.copyfile(path_to_base_mpq_file, temp_mpq_file.name)
             open_result = self._stormlib_wrapper.open_archive(
@@ -67,3 +77,13 @@ class StarcraftMpqIo:
                 self._stormlib_wrapper.compact_archive(open_result)
             )
             shutil.copyfile(temp_mpq_file.name, path_to_new_mpq_file)
+
+    def _build_wav_metadata_lookup(
+        self, path_to_base_mpq_file: str
+    ) -> RichWavMetadataLookup:
+        wav_metadata = StarcraftWavMetadataIo(
+            stormlib_wrapper=self._stormlib_wrapper
+        ).extract_all_wav_files_metadata(path_to_base_mpq_file)
+        return RichWavMetadataLookup(
+            _metadata_by_wav_path={x.path_to_wav_in_mpq: x for x in wav_metadata}
+        )
