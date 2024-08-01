5ff73dbff56d9fa26ca068c3d268ff22208318ad
sethmachine <sethmachine01@gmail.com>
2024-08-01 19:51:13 -0400

    create play wav action

---
+++ src/richchk/model/richchk/wav/rich_wav_lookup.py
@@ -0,0 +1,20 @@
+"""Resolve WAV IDs.
+
+This is primarily used to expose the WAV file's millisecond duration for automatic
+completion in trigger actions.
+"""
+
+import dataclasses
+from typing import Optional
+
+from ...mpq.stormlib.wav.stormlib_wav import StormLibWav
+
+
+@dataclasses.dataclass(frozen=True)
+class RichWavMetadataLookup:
+    _metadata_by_wav_path: dict[str, StormLibWav]
+
+    def get_metadata_by_wav_path(
+        self, path_to_wav_in_mpq: str
+    ) -> Optional[StormLibWav]:
+        return self._metadata_by_wav_path.get(path_to_wav_in_mpq, None)
