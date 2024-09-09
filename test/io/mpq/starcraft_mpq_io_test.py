3c8f5d6bfc3a5d96296f1f412f95a357977c3a23
sethmachine <sethmachine01@gmail.com>
2024-09-08 20:55:32 -0400

    write integration test to verify wav duration is being set when not specified

--- test/io/mpq/starcraft_mpq_io_test.py
+++ test/io/mpq/starcraft_mpq_io_test.py
@@ -4,9 +4,18 @@ import uuid
 
 import pytest
 
+from richchk.editor.richchk.rich_chk_editor import RichChkEditor
+from richchk.editor.richchk.rich_trig_editor import RichTrigEditor
 from richchk.io.mpq.starcraft_mpq_io import StarcraftMpqIo
+from richchk.io.mpq.starcraft_wav_metadata_io import StarcraftWavMetadataIo
+from richchk.io.util.chk_query_util import ChkQueryUtil
 from richchk.model.mpq.stormlib.stormlib_file_path import StormLibFilePath
 from richchk.model.richchk.rich_chk import RichChk
+from richchk.model.richchk.trig.actions.play_wav_action import PlayWavAction
+from richchk.model.richchk.trig.conditions.always_condition import AlwaysCondition
+from richchk.model.richchk.trig.player_id import PlayerId
+from richchk.model.richchk.trig.rich_trig_section import RichTrigSection
+from richchk.model.richchk.trig.rich_trigger import RichTrigger
 from richchk.mpq.stormlib.stormlib_loader import StormLibLoader
 from richchk.mpq.stormlib.stormlib_wrapper import StormLibWrapper
 
@@ -37,6 +46,12 @@ def stormlib_wrapper():
 def mpq_io(stormlib_wrapper):
     if stormlib_wrapper:
         return StarcraftMpqIo(stormlib_wrapper)
+
+
+@pytest.fixture(scope="function")
+def wav_metadata_io(stormlib_wrapper):
+    if stormlib_wrapper:
+        return StarcraftWavMetadataIo(stormlib_wrapper)
 
 
 def _read_file_as_bytes(infile: str) -> bytes:
@@ -126,8 +141,50 @@ def test_it_throws_adding_chk_if_no_overwrite_and_outfile_exists(mpq_io):
                 )
 
 
-def test_integration_it_adds_play_wav_action_without_duration(mpq_io):
+def test_integration_it_adds_play_wav_action_without_duration(mpq_io, wav_metadata_io):
     # a PlayWavAction can be added if it references an existing WAV file without duration specified
     # since the duration can be pulled from the WAV metadata
-    if mpq_io:
-        pass
+    if mpq_io and wav_metadata_io:
+        with (
+            tempfile.NamedTemporaryFile() as temp_scx_file,
+            tempfile.NamedTemporaryFile() as temp_scx_new_mpq,
+        ):
+            shutil.copy(EXAMPLE_STARCRAFT_SCX_MAP, temp_scx_file.name)
+            chk = mpq_io.read_chk_from_mpq(temp_scx_file.name)
+            # this wavfile exists in the CHK already
+            wavfile_in_chk = "staredit\\wav\\monitor humming.1.wav"
+            play_wav_trig = RichTrigger(
+                _conditions=[AlwaysCondition()],
+                _actions=[
+                    # do not specify duration, since it will be automatically calculated when saving the CHK
+                    PlayWavAction(_path_to_wav_in_mpq=wavfile_in_chk, _duration_ms=None)
+                ],
+                _players={PlayerId.PLAYER_1},
+            )
+            updated_trig = RichTrigEditor.add_triggers(
+                [play_wav_trig],
+                ChkQueryUtil.find_only_rich_section_in_chk(RichTrigSection, chk),
+            )
+            updated_chk = RichChkEditor().replace_chk_section(updated_trig, chk)
+            mpq_io.save_chk_to_mpq(
+                updated_chk, temp_scx_file.name, temp_scx_new_mpq.name, True
+            )
+            # now read back the updated CHK and verify the play wav trigger has the duration we expect
+            updated_chk_again = mpq_io.read_chk_from_mpq(temp_scx_new_mpq.name)
+            # find the play wav trigger, there's only a single one so this is safe for now
+            expected_playwav_trig = [
+                trig
+                for trig in ChkQueryUtil.find_only_rich_section_in_chk(
+                    RichTrigSection, updated_chk_again
+                ).triggers
+                if isinstance(trig.actions[0], PlayWavAction)
+            ][0]
+            play_wav_action = expected_playwav_trig.actions[0]
+            assert isinstance(play_wav_action, PlayWavAction)
+            assert play_wav_action.path_to_wav_in_mpq == wavfile_in_chk
+            # now assert the duration is filled in as expected
+            # the exact duration milliseconds of the WAV file as stored in the CHK
+            expected_duration = wav_metadata_io.extract_all_wav_files_metadata(
+                temp_scx_file.name
+            )[0].duration_ms
+            assert play_wav_action.duration_ms == expected_duration
