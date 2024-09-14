f132982a995a8c51a1e7eef0ec062cfcd0828f1a
sethmachine <sethmachine01@gmail.com>
2024-09-14 12:14:20 -0400

    add comment explaining move location action

--- src/richchk/model/richchk/trig/actions/move_location_action.py
+++ src/richchk/model/richchk/trig/actions/move_location_action.py
@@ -1,3 +1,12 @@
+"""Center a location on a unit type owned by a player in another location.
+
+This how locations can be "moved" around the map.  This centers the "source_location" on
+a unit type owned by another player (or force/group) which is located in the
+"destination_location."
+
+Often times the "destination_location" is "Anywhere", but can be more restrictive
+depending upon needs or how the map may be divided.
+"""
 import dataclasses
 from abc import ABC
 
