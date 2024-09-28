6cb9383e708239be42ab9383f0ce07f5b6898669
sethmachine <sethmachine01@gmail.com>
2024-09-27 23:02:35 -0400

    rename from Starcraft to StarCraft

--- test/io/mpq/starcraft_mpq_io_helper_test.py
+++ test/io/mpq/starcraft_mpq_io_helper_test.py
@@ -1,6 +1,6 @@
 import platform
 
-from richchk.io.mpq.starcraft_mpq_io_helper import StarcraftMpqIoHelper
+from richchk.io.mpq.starcraft_mpq_io_helper import StarCraftMpqIoHelper
 
 from ...chk_resources import MACOS_STORMLIB_M1
 
@@ -13,31 +13,31 @@ def _run_test_if_mac_m1() -> bool:
 
 def test_it_create_mpq_io_from_embedded_dll():
     if _run_test_if_mac_m1():
-        StarcraftMpqIoHelper.create_mpq_io(path_to_stormlib_dll=None)
+        StarCraftMpqIoHelper.create_mpq_io(path_to_stormlib_dll=None)
 
 
 def test_it_create_mpq_io_from_provided_dll():
     if _run_test_if_mac_m1():
-        StarcraftMpqIoHelper.create_mpq_io(path_to_stormlib_dll=MACOS_STORMLIB_M1)
+        StarCraftMpqIoHelper.create_mpq_io(path_to_stormlib_dll=MACOS_STORMLIB_M1)
 
 
 def test_it_create_wav_io_from_embedded_dll():
     if _run_test_if_mac_m1():
-        StarcraftMpqIoHelper.create_wav_io(path_to_stormlib_dll=None)
+        StarCraftMpqIoHelper.create_wav_io(path_to_stormlib_dll=None)
 
 
 def test_it_create_wav_io_from_provided_dll():
     if _run_test_if_mac_m1():
-        StarcraftMpqIoHelper.create_wav_io(path_to_stormlib_dll=MACOS_STORMLIB_M1)
+        StarCraftMpqIoHelper.create_wav_io(path_to_stormlib_dll=MACOS_STORMLIB_M1)
 
 
 def test_it_create_wav_metadata_io_from_embedded_dll():
     if _run_test_if_mac_m1():
-        StarcraftMpqIoHelper.create_wav_metadata_io(path_to_stormlib_dll=None)
+        StarCraftMpqIoHelper.create_wav_metadata_io(path_to_stormlib_dll=None)
 
 
 def test_it_create_wav_metadata_io_from_provided_dll():
     if _run_test_if_mac_m1():
-        StarcraftMpqIoHelper.create_wav_metadata_io(
+        StarCraftMpqIoHelper.create_wav_metadata_io(
             path_to_stormlib_dll=MACOS_STORMLIB_M1
         )
