e140b2fd17c97902614bc8c471d42f2f9a7b3220
sethmachine <sethmachine01@gmail.com>
2024-10-21 18:36:13 -0400

    remove custom temp file manage

--- src/richchk/util/fileutils.py
+++ src/richchk/util/fileutils.py
@@ -2,42 +2,7 @@
 
 import os
 import re
-import tempfile
-import typing
 from typing import Iterator
-
-
-class CrossPlatformSafeTemporaryNamedFile:
-    """This custom implementation is needed because of the following limitation of
-    tempfile.NamedTemporaryFile:
-
-    > Whether the name can be used to open the file a second time, while the named
-    temporary file is still open, > varies across platforms (it can be so used on Unix;
-    it cannot on Windows NT or later).
-
-    Taken from:
-    https://stackoverflow.com/questions/23212435/permission-denied-to-write-to-my-temporary-file
-    """
-
-    def __init__(self, mode: str = "wb", delete: bool = True) -> None:
-        self._mode = mode
-        self._delete = delete
-
-    def __enter__(self) -> typing.IO[typing.Any]:
-        # Generate a random temporary file name
-        file_name = os.path.join(tempfile.gettempdir(), os.urandom(24).hex())
-        # Ensure the file is created
-        open(file_name, "x").close()
-        # Open the file in the given mode
-        self._tempFile = open(file_name, self._mode)
-        return self._tempFile
-
-    def __exit__(
-        self, exc_type: typing.Any, exc_val: typing.Any, exc_tb: typing.Any
-    ) -> None:
-        self._tempFile.close()
-        if self._delete:
-            os.remove(self._tempFile.name)
 
 
 def absolute_filepaths(
