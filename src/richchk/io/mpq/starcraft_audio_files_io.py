f345a0c4f26be232878fec6e464fa1a48a6217ec
sethmachine <sethmachine01@gmail.com>
2025-03-30 09:00:49 -0400

    rename wav mpq classes to more generic audio name

--- src/richchk/io/mpq/starcraft_audio_files_io.py
+++ src/richchk/io/mpq/starcraft_audio_files_io.py
@@ -1,4 +1,4 @@
-"""Add WAV files to a StarCraft MPQ and update the WAV/STR sections in the CHK."""
+"""Add audio files to a StarCraft MPQ and update the WAV/STR sections in the CHK."""
 import os
 import shutil
 
@@ -13,7 +13,7 @@ from ..richchk.query.chk_query_util import ChkQueryUtil
 from .starcraft_mpq_io import StarCraftMpqIo
 
 
-class StarCraftWavIo:
+class StarCraftAudioFilesIo:
 
     # canonical location where WAV files are stored in a StarCraft MPQ
     _WAV_DIRECTORY = "staredit\\wav"
@@ -22,9 +22,9 @@ class StarCraftWavIo:
         self._stormlib_wrapper = stormlib_wrapper
         self._starcraft_mpq_io = StarCraftMpqIo(stormlib_wrapper=stormlib_wrapper)
 
-    def add_wav_files_to_mpq(
+    def add_audio_files_to_mpq(
         self,
-        path_to_wavs_on_disk: list[str],
+        path_to_audio_files_on_disk: list[str],
         path_to_base_mpq_file: str,
         path_to_new_mpq_file: str,
         overwrite_existing: bool = False,
@@ -32,15 +32,15 @@ class StarCraftWavIo:
         """Adds WAV files to the Starcraft MPQ, also updating the WAV and STR sections
         in the CHK.
 
-        :param path_to_wavs_on_disk:
+        :param path_to_audio_files_on_disk:
         :param path_to_base_mpq_file:
         :param path_to_new_mpq_file:
         :param overwrite_existing:
         :return:
         """
-        if not all((os.path.exists(wav) for wav in path_to_wavs_on_disk)):
+        if not all((os.path.exists(wav) for wav in path_to_audio_files_on_disk)):
             raise FileNotFoundError(
-                f"At least one WAV file does not exist: {path_to_wavs_on_disk}"
+                f"At least one WAV file does not exist: {path_to_audio_files_on_disk}"
             )
         if not os.path.exists(path_to_base_mpq_file):
             raise FileNotFoundError(path_to_base_mpq_file)
@@ -50,9 +50,9 @@ class StarCraftWavIo:
             )
         with CrossPlatformSafeTemporaryNamedFile() as temp_mpq_file:
             shutil.copyfile(path_to_base_mpq_file, temp_mpq_file)
-            new_wav_files = self._add_wavfiles_to_mpq(
+            new_wav_files = self._add_audio_files_to_mpq(
                 path_to_mpq_file=temp_mpq_file,
-                path_to_wavs_on_disk=path_to_wavs_on_disk,
+                path_to_wavs_on_disk=path_to_audio_files_on_disk,
             )
             new_chk = self._update_chk_wav_section(temp_mpq_file, new_wav_files)
             self._starcraft_mpq_io.save_chk_to_mpq(
@@ -62,7 +62,7 @@ class StarCraftWavIo:
                 overwrite_existing=True,
             )
 
-    def _add_wavfiles_to_mpq(
+    def _add_audio_files_to_mpq(
         self,
         path_to_mpq_file: str,
         path_to_wavs_on_disk: list[str],
@@ -78,7 +78,7 @@ class StarCraftWavIo:
         )
         path_to_wavs_in_mpq = []
         for wavfile in path_to_wavs_on_disk:
-            wavfile_in_mpq = self._create_wav_filepath_in_mpq(wavfile)
+            wavfile_in_mpq = self._create_audio_filepath_in_mpq(wavfile)
             path_to_wavs_in_mpq.append(wavfile_in_mpq)
             self._stormlib_wrapper.add_file(
                 open_archive_result,
@@ -91,7 +91,7 @@ class StarCraftWavIo:
         return path_to_wavs_in_mpq
 
     @classmethod
-    def _create_wav_filepath_in_mpq(cls, path_to_wav_on_disk: str) -> str:
+    def _create_audio_filepath_in_mpq(cls, path_to_wav_on_disk: str) -> str:
         bn = os.path.basename(path_to_wav_on_disk)
         return cls._WAV_DIRECTORY + "\\" + bn
 
@@ -99,11 +99,11 @@ class StarCraftWavIo:
         self, path_to_mpq_file: str, new_wav_files: list[str]
     ) -> RichChk:
         chk = self._starcraft_mpq_io.read_chk_from_mpq(path_to_mpq_file)
-        new_wav = self._update_wav_with_new_wav_files(new_wav_files, chk)
+        new_wav = self._update_wav_with_new_audio_files(new_wav_files, chk)
         new_chk = RichChkEditor().replace_chk_section(new_wav, chk)
         return new_chk
 
-    def _update_wav_with_new_wav_files(
+    def _update_wav_with_new_audio_files(
         self, new_wav_files: list[str], chk: RichChk
     ) -> RichWavSection:
         rich_wav = ChkQueryUtil.find_only_rich_section_in_chk(RichWavSection, chk)
--- test/io/mpq/stacraft_wav_io_test.py
+++ test/io/mpq/stacraft_wav_io_test.py
@@ -3,9 +3,11 @@ import shutil
 
 import pytest
 
+from richchk.io.mpq.starcraft_audio_files_io import StarCraftAudioFilesIo
+from richchk.io.mpq.starcraft_audio_files_metadata_io import (
+    StarCraftAudioFilesMetadataIo,
+)
 from richchk.io.mpq.starcraft_mpq_io import StarCraftMpqIo
-from richchk.io.mpq.starcraft_wav_io import StarCraftWavIo
-from richchk.io.mpq.starcraft_wav_metadata_io import StarCraftWavMetadataIo
 from richchk.io.richchk.query.chk_query_util import ChkQueryUtil
 from richchk.model.chk.str.decoded_str_section import DecodedStrSection
 from richchk.model.mpq.stormlib.stormlib_archive_mode import StormLibArchiveMode
@@ -52,7 +54,7 @@ def mpq_io(stormlib_wrapper):
 @pytest.fixture(scope="function")
 def wav_io(stormlib_wrapper):
     if stormlib_wrapper:
-        return StarCraftWavIo(stormlib_wrapper)
+        return StarCraftAudioFilesIo(stormlib_wrapper)
 
 
 def _read_file_as_bytes(infile: str) -> bytes:
@@ -67,7 +69,7 @@ def test_integration_it_adds_a_wav_file_to_mpq(mpq_io, wav_io, stormlib_wrapper)
             CrossPlatformSafeTemporaryNamedFile() as new_scx_file,
         ):
             shutil.copy(EXAMPLE_STARCRAFT_SCX_MAP, temp_scx_file)
-            wav_io.add_wav_files_to_mpq(
+            wav_io.add_audio_files_to_mpq(
                 [EXAMPLE_WAV_FILE.as_posix()],
                 temp_scx_file,
                 new_scx_file,
@@ -96,7 +98,7 @@ def test_integration_it_adds_an_ogg_file_to_mpq(mpq_io, wav_io, stormlib_wrapper
             CrossPlatformSafeTemporaryNamedFile() as new_scx_file,
         ):
             shutil.copy(EXAMPLE_STARCRAFT_SCX_MAP, temp_scx_file)
-            wav_io.add_wav_files_to_mpq(
+            wav_io.add_audio_files_to_mpq(
                 [EXAMPLE_OGG_FILE.as_posix()],
                 temp_scx_file,
                 new_scx_file,
@@ -157,8 +159,8 @@ def _assert_wav_metadata_exists(
 ):
     wav_metadata = {
         x.path_to_wav_in_mpq: x
-        for x in StarCraftWavMetadataIo(
+        for x in StarCraftAudioFilesMetadataIo(
             stormlib_wrapper
-        ).extract_all_wav_files_metadata(path_to_mpq_file)
+        ).extract_all_audio_files_metadata(path_to_mpq_file)
     }
     assert wav_file_in_mpq in wav_metadata
